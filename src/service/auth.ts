6f03b04f859caec209fb44a78d976750aa56e882
buivantoana <toanbui219@gmail.com>
2024-11-03 11:01:12 +0700

    .

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -21,7 +21,19 @@ export async function signIn(auth_code: any) {
     console.log("AAAA reponse ===", response);
     return response.data;
   } catch (error: any) {
-    console.error("Error fetching data:", error.message);
+    if (error.response) {
+      // The request was made, and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error("Error response data:", error.response.data);
+      console.error("Error response status:", error.response.status);
+      return error.response.data; // You can return this to handle error responses
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error("No response received:", error.request);
+    } else {
+      // Something happened in setting up the request that triggered an error
+      console.error("Error setting up request:", error.message);
+    }
   }
 }
 export async function getOtp(phone: any) {
