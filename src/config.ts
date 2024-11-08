98930d69629e612d733d71d2c285d30e8d307bef
buivantoana <toanbui219@gmail.com>
2024-11-08 21:09:07 +0700

    .

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const url_auth = "https://dev-koc.gmv.vn";
-export const url_voice = "https://vp.zeezoo.mobi";
+export const url_voice = "https://45.77.191.158";
 export const path_redirect = "https://web-voice.vercel.app/auth/callback";
