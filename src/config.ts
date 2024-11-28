2288540126d2c0feec35bd52e73acde408141ba2
buivantoana <toanbui219@gmail.com>
2024-11-28 22:25:12 +0700

    .

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const url_auth = "https://dev-koc.gmv.vn";
-export const url_voice = "https://ai.gmv.vn/api";
+export const url_voice = "https://api.ttsopenai.zeezoo.mobi/api";
 export const path_redirect = "https://web-voice.vercel.app/auth/callback";
