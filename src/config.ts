bd9482019dec03a2795c3008ecf343867435505d
buivantoana <106064775+buivantoana@users.noreply.github.com>
2024-12-04 08:59:29 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const url_auth = "https://dev-koc.gmv.vn";
-export const url_voice = "https://vp.zeezoo.mobi/api";
+export const url_voice = "https://api.ttsopenai.zeezoo.mobi/api";
 export const path_redirect = "https://web-voice.vercel.app/auth/callback";
