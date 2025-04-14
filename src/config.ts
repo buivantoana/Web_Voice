f802d9f94e2802e3b13c6d4d751563a63e7b7868
Bui Toan <toan.bui@crmi.vn>
2025-04-14 08:54:52 +0700

    .

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const url_auth = "https://dev-koc.gmv.vn";
 export const url_voice = "https://dev.ttsopenai.zeezoo.mobi/api";
-export const path_redirect = "https://ai.gmv.vn/auth/callback";
+export const path_redirect = "https://web-voice.vercel.app/auth/callback";
